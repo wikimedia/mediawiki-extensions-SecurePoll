91c93f1fd2bf0b7e1bf56148a1461a23a393a2b0
Reedy <reedy@wikimedia.org>
2013-06-15 02:03:04 +0000

    Revert "Fix parameter list order when splitting a string"
    This reverts commit 8c9b903e73ec27f88dfebe3298f8002b4ffc2b01
    
    Change-Id: I7d69a4896d816bd701dba0d1b5191fc7c6848caa

--- cli/wm-scripts/bv2013/sendMails.php
+++ cli/wm-scripts/bv2013/sendMails.php
@@ -30,7 +30,7 @@ while ( !is_null( $line = fgets( $in ) ) ) {
 	if ( !$line ) {
 		continue;
 	}
-	list( $site, $name, $address, $lang ) = explode( "\t", trim( $line ) );
+	list( $address, $lang, $site, $name ) = explode( "\t", trim( $line ) );
 
 	if ( !( $name && $lang && $address && $site ) ) {
 		print "invalid line $line $name $lang $address $site\n";
