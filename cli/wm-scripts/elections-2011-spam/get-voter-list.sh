67c4d61f5f1d0121c104560a71b9ded4472ac5d0
Reedy <reedy@wikimedia.org>
2013-07-11 21:51:49 +0100

    Kill old election/vote scripts
    Change-Id: Iefc6062fa5062e340378d7825a1dc11186465755

--- cli/wm-scripts/elections-2011-spam/get-voter-list.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for wiki in `</a/common/all.dblist`; do
-	echo $wiki;
-	echo "SELECT user_email, up_value, '$wiki', user_name FROM securepoll_lists JOIN user ON li_member=user_id JOIN user_properties ON up_property='language' AND up_user=user_id WHERE li_name='board-vote-2011' AND user_email_authenticated IS NOT NULL;" \
-		| sql $wiki | tail -n +2 >>/home/andrew/elections-2011-spam/users-by-wiki;
-done
