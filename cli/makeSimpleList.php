d6e346344b26da362d63fa5750b7beabaeedc65c
Reedy <reedy@wikimedia.org>
2013-11-19 21:40:55 +0000

    Add a bit of output to makeSimpleList.php
    Change-Id: I8c0df54129fd57effeedb01dc4dd31fc2325414a

--- cli/makeSimpleList.php
+++ cli/makeSimpleList.php
@@ -35,11 +35,13 @@ if ( $listExists ) {
 	}
 }
 
+$total = 0;
 while ( true ) {
 	$res = $dbr->select( 'user', 'user_id',
 		array( 'user_id > ' . $dbr->addQuotes( $startBatch ) ),
 		$fname,
-		array( 'LIMIT' => $batchSize ) );
+		array( 'LIMIT' => $batchSize )
+	);
 
 	if ( !$res->numRows() ) {
 		break;
@@ -65,6 +67,11 @@ while ( true ) {
 		}
 	}
 	if ( $insertBatch ) {
+		$count = count( $insertBatch );
+		$total += $count;
+		echo "$count rows inserted, total $total rows";
 		$dbw->insert( 'securepoll_lists', $insertBatch, $fname );
 	}
 }
+
+echo "Done!\n";
