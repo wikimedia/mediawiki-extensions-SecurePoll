62b84a5ecedc4e37a5031b238277e827fff000c0
Brad Jorsch <bjorsch@wikimedia.org>
2014-11-13 12:32:55 -0500

    Edit translations for all jump wikis on central wiki
    Fortunately jump wikis don't have questions or options, so we only have
    to worry about mapping the election's ID (which we do in the same way we
    do elsewhere, by title lookup).
    
    Bug: T74576
    Change-Id: Ifcf2ac4b0f9544a3968df52c2f653f15ee2f6426

--- includes/pages/TranslatePage.php
+++ includes/pages/TranslatePage.php
@@ -26,6 +26,31 @@ class SecurePoll_TranslatePage extends SecurePoll_Page {
 		$wgOut->setPageTitle( wfMsg( 'securepoll-translate-title',
 			$this->election->getMessage( 'title' ) ) );
 
+		$jumpUrl = $this->election->getProperty( 'jump-url' );
+		if ( $jumpUrl ) {
+			$jumpId = $this->election->getProperty( 'jump-id' );
+			if ( !$jumpId ) {
+				throw new MWException( 'Configuration error: no jump-id' );
+			}
+			$jumpUrl .= "/edit/$jumpId";
+			if ( count( $params ) > 1 ) {
+				$jumpUrl .= '/' . join( '/', array_slice( $params, 1 ) );
+			}
+
+			$wiki = $this->election->getProperty( 'main-wiki' );
+			if ( $wiki ) {
+				$wiki = WikiMap::getWikiName( $wiki );
+			} else {
+				$wiki = $this->msg( 'securepoll-edit-redirect-otherwiki' )->text();
+			}
+
+			$wgOut->addWikiMsg( 'securepoll-edit-redirect',
+				Message::rawParam( Linker::makeExternalLink( $jumpUrl, $wiki ) )
+			);
+
+			return;
+		}
+
 		$this->isAdmin = $this->election->isAdmin( $wgUser );
 
 		$primary = $this->election->getLanguage();
@@ -164,6 +189,7 @@ class SecurePoll_TranslatePage extends SecurePoll_Page {
 
 		$entities = array_merge( array( $this->election ), $this->election->getDescendants() );
 		$replaceBatch = array();
+		$jumpReplaceBatch = array();
 		foreach ( $entities as $entity ) {
 			foreach ( $entity->getMessageNames() as $messageName ) {
 				$controlName = 'trans_' . $entity->getId() . '_' . $messageName;
@@ -175,10 +201,28 @@ class SecurePoll_TranslatePage extends SecurePoll_Page {
 						'msg_key' => $messageName,
 						'msg_text' => $value
 					);
+
+					// Jump wikis don't have subentities
+					if ( $entity === $this->election ) {
+						$jumpReplaceBatch[] = array(
+							'msg_entity' => $entity->getId(),
+							'msg_lang' => $secondary,
+							'msg_key' => $messageName,
+							'msg_text' => $value
+						);
+					}
 				}
 			}
 		}
 		if ( $replaceBatch ) {
+			$wikis = $this->election->getProperty( 'wikis' );
+			if ( $wikis ) {
+				$wikis = explode( "\n", $wikis );
+			} else {
+				$wikis = array();
+			}
+
+			// First, the main wiki
 			$dbw = $this->context->getDB();
 			$dbw->replace(
 				'securepoll_msgs',
@@ -196,6 +240,38 @@ class SecurePoll_TranslatePage extends SecurePoll_Page {
 					$election, "msg/$secondary" );
 				$wp = WikiPage::factory( $title );
 				$wp->doEditContent( $content, $wgRequest->getText( 'comment' ) );
+			}
+
+			// Then each jump-wiki
+			foreach ( $wikis as $dbname ) {
+				if ( $dbname === wfWikiID() ) {
+					continue;
+				}
+
+				$lb = wfGetLB( $dbname );
+				$dbw = $lb->getConnection( DB_MASTER, array(), $dbname );
+				try {
+					$id = $dbw->selectField( 'securepoll_elections', 'el_entity', array(
+						'el_title' => $this->election->title
+					) );
+					if ( $id ) {
+						foreach ( $jumpReplaceBatch as &$row ) {
+							$row['msg_entity'] = $id;
+						}
+						unset( $row );
+
+						$dbw->replace(
+							'securepoll_msgs',
+							array( array( 'msg_entity', 'msg_lang', 'msg_key' ) ),
+							$jumpReplaceBatch,
+							__METHOD__
+						);
+					}
+				} catch ( Exception $ex ) {
+					// Log the exception, but don't abort the updating of the rest of the jump-wikis
+					MWExceptionHandler::logException( $ex );
+				}
+				$lb->reuseConnection( $dbw );
 			}
 		}
 		$wgOut->redirect( $this->getTitle( $secondary )->getFullUrl() );
