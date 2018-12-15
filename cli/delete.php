ab1439e807543d0096abb34375d54ccef2d00447
Reedy <reedy@wikimedia.org>
2018-12-15 16:03:23 +0000

    Convert delete.php
    Bug: T212027
    Change-Id: Ib1080b6e80bf7cd637cea9267d915c18984c54b0

--- cli/delete.php
+++ cli/delete.php
@@ -1,79 +1,81 @@
 <?php
 
-require __DIR__ . '/cli.inc';
-
-$usage = <<<EOT
-Delete a poll from the local SecurePoll database.
-
-Usage: delete.php <id>
-
-EOT;
-
-if ( !isset( $args[0] ) ) {
-	echo $usage;
-	exit( 1 );
+if ( getenv( 'MW_INSTALL_PATH' ) ) {
+	$IP = getenv( 'MW_INSTALL_PATH' );
+} else {
+	$IP = __DIR__ . '/../../..';
 }
-$id = intval( $args[0] );
-if ( $args[0] !== (string)$id ) {
-	echo "The specified id \"{$args[0]}\" is not an integer\n";
-	exit( 1 );
+require_once "$IP/maintenance/Maintenance.php";
+
+class DeletePoll extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'Delete a poll from the local SecurePoll database';
+
+		$this->addArg( 'id', 'Secure Poll id to delete' );
+	}
+
+	public function execute() {
+		$electionId = (int)$this->getArg();
+
+		$dbw = wfGetDB( DB_MASTER );
+
+		$type = $dbw->selectField(
+			'securepoll_entity',
+			'en_type',
+			[ 'en_id' => $electionId ],
+			__METHOD__,
+			[ 'FOR UPDATE' ]
+		);
+
+		if ( !$type ) {
+			$this->fatalError( "The specified id does not exist.\n" );
+		}
+		if ( $type !== 'election' ) {
+			$this->fatalError( "The specified id is for an entity of type \"$type\", not \"election\".\n" );
+		}
+
+		# Get a list of entity IDs and lock them
+		$res = $dbw->select(
+			'securepoll_questions',
+			[ 'qu_entity' ],
+			[ 'qu_election' => $electionId ],
+			__METHOD__,
+			[ 'FOR UPDATE' ]
+		);
+		$questionIds = [];
+		foreach ( $res as $row ) {
+			$questionIds[] = $row->qu_entity;
+		}
+
+		$res = $dbw->select(
+			'securepoll_options',
+			[ 'op_entity' ],
+			[ 'op_election' => $electionId ],
+			__METHOD__,
+			[ 'FOR UPDATE' ]
+		);
+		$optionIds = [];
+		foreach ( $res as $row ) {
+			$optionIds[] = $row->op_entity;
+		}
+
+		$entityIds = array_merge( $optionIds, $questionIds, [ $electionId ] );
+
+		# Delete the messages and properties
+		$dbw->delete( 'securepoll_msgs', [ 'msg_entity' => $entityIds ] );
+		$dbw->delete( 'securepoll_properties', [ 'pr_entity' => $entityIds ] );
+
+		# Delete the entities
+		if ( $optionIds ) {
+			$dbw->delete( 'securepoll_options', [ 'op_entity' => $optionIds ], __METHOD__ );
+		}
+		if ( $questionIds ) {
+			$dbw->delete( 'securepoll_questions', [ 'qu_entity' => $questionIds ], __METHOD__ );
+		}
+		$dbw->delete( 'securepoll_elections', [ 'el_entity' => $electionId ], __METHOD__ );
+		$dbw->delete( 'securepoll_entity', [ 'en_id' => $entityIds ], __METHOD__ );
+	}
 }
-
-$success = spDeleteElection( $id );
-exit( $success ? 0 : 1 );
-
-/**
- * @suppress SecurityCheck-XSS
- * @param int|string $electionId
- * @return bool
- */
-function spDeleteElection( $electionId ) {
-	$dbw = wfGetDB( DB_MASTER );
-
-	$type = $dbw->selectField( 'securepoll_entity', 'en_type',
-		[ 'en_id' => $electionId ],
-		__METHOD__, [ 'FOR UPDATE' ] );
-	if ( !$type ) {
-		echo "The specified id does not exist.\n";
-		return false;
-	}
-	if ( $type !== 'election' ) {
-		echo "The specified id is for an entity of type \"$type\", not \"election\".\n";
-		return false;
-	}
-
-	# Get a list of entity IDs and lock them
-	$questionIds = [];
-	$res = $dbw->select( 'securepoll_questions', [ 'qu_entity' ],
-		[ 'qu_election' => $electionId ],
-		__METHOD__, [ 'FOR UPDATE' ] );
-	foreach ( $res as $row ) {
-		$questionIds[] = $row->qu_entity;
-	}
-
-	$res = $dbw->select( 'securepoll_options', [ 'op_entity' ],
-		[ 'op_election' => $electionId ],
-		__METHOD__, [ 'FOR UPDATE' ] );
-	$optionIds = [];
-	foreach ( $res as $row ) {
-		$optionIds[] = $row->op_entity;
-	}
-
-	$entityIds = array_merge( $optionIds, $questionIds, [ $electionId ] );
-
-	# Delete the messages and properties
-	$dbw->delete( 'securepoll_msgs', [ 'msg_entity' => $entityIds ] );
-	$dbw->delete( 'securepoll_properties', [ 'pr_entity' => $entityIds ] );
-
-	# Delete the entities
-	if ( $optionIds ) {
-		$dbw->delete( 'securepoll_options', [ 'op_entity' => $optionIds ], __METHOD__ );
-	}
-	if ( $questionIds ) {
-		$dbw->delete( 'securepoll_questions', [ 'qu_entity' => $questionIds ], __METHOD__ );
-	}
-	$dbw->delete( 'securepoll_elections', [ 'el_entity' => $electionId ], __METHOD__ );
-	$dbw->delete( 'securepoll_entity', [ 'en_id' => $entityIds ], __METHOD__ );
-
-	return true;
-}
+$maintClass = "DeletePoll";
+require_once RUN_MAINTENANCE_IF_MAIN;
