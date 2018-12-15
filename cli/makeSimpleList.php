b85ec2b211748c47d1ef4d98e32b7d3f2581ee89
Reedy <reedy@wikimedia.org>
2018-12-15 16:14:13 +0000

    Convert makeSimpleList.php
    Also remove conditional code that requires older MW versions.
    extension.json requires MW >= 1.32
    
    Bug: T212027
    Change-Id: I7365cc508378a5bfb2952d599f22e5336e583b7d

--- cli/makeSimpleList.php
+++ cli/makeSimpleList.php
@@ -13,140 +13,131 @@
  *                      to be resumed.
  */
 
-$optionsWithArgs = [ 'before', 'edits', 'start-from' ];
-require __DIR__ . '/cli.inc';
-
-$dbr = wfGetDB( DB_REPLICA );
-$dbw = wfGetDB( DB_MASTER );
-$fname = 'makeSimpleList.php';
-$before = isset( $options['before'] ) ? $dbr->timestamp( strtotime( $options['before'] ) ) : false;
-$minEdits = isset( $options['edits'] ) ? intval( $options['edits'] ) : false;
-
-if ( !isset( $args[0] ) ) {
-	echo <<<EOD
-Generate a list of users with some number of edits before some date.
-
-Usage: php makeSimpleList.php [OPTIONS] LIST_NAME
-  --replace          If list exists, delete it and recreate.
-  --ignore-existing  Leave existing list items in place.
-  --edits=COUNT      Edit count required for eligibility.
-  --before=DATE      Consider edits made before DATE (strtotime format).
-  --mainspace-only   Consider only NS_MAIN edits.
-  --start-from=UID   Start from user ID UID. Allows crashed invocations
-                     to be resumed.
-
-EOD;
-	exit( 1 );
-}
-$listName = $args[0];
-$startBatch = isset( $options['start-from'] ) ? $options['start-from'] : 0;
-$batchSize = 100;
-$insertOptions = [];
-
-$listExists = $dbr->selectField( 'securepoll_lists', '1',
-	[ 'li_name' => $listName ], $fname );
-if ( $listExists ) {
-	if ( isset( $options['replace'] ) ) {
-		echo "Deleting existing list...\n";
-		$dbw->delete( 'securepoll_lists', [ 'li_name' => $listName ], $fname );
-	} elseif ( isset( $options['ignore-existing'] ) ) {
-		$insertOptions[] = 'IGNORE';
-	} else {
-		echo "Error: list exists. Use --replace to replace it.\n";
-		exit( 1 );
-	}
-}
-
-if ( is_callable( [ 'User', 'getQueryInfo' ] ) ) {
-	$userQuery = User::getQueryInfo();
+if ( getenv( 'MW_INSTALL_PATH' ) ) {
+	$IP = getenv( 'MW_INSTALL_PATH' );
 } else {
-	$userQuery = [
-		'tables' => [ 'user' ],
-		'fields' => User::selectFields(),
-		'joins' => [],
-	];
+	$IP = __DIR__ . '/../../..';
 }
-if ( !class_exists( 'ActorMigration' ) ) {
-	// We only need user_id in this case
-	$userQuery['fields'] = [ 'user_id' ];
-}
+require_once "$IP/maintenance/Maintenance.php";
 
-$beforeQ = $before !== false ? $dbr->addQuotes( $before ) : false;
+class MakeSimpleList extends Maintenance {
 
-while ( true ) {
-	echo "user_id > $startBatch\n";
-	$res = $dbr->select(
-		$userQuery['tables'],
-		$userQuery['fields'],
-		[ 'user_id > ' . $dbr->addQuotes( $startBatch ) ],
-		$fname,
-		[ 'LIMIT' => $batchSize ],
-		$userQuery['joins']
-	);
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'Generate a list of users with some number of edits before some date';
 
-	if ( !$res->numRows() ) {
-		break;
+		$this->addArg( 'listname', 'Name of the list' );
+		$this->addOption( 'replace', 'If list exists, delete it and recreate' );
+		$this->addOption( 'ignore-existing', 'Leave existing list items in place' );
+		$this->addOption( 'edits', 'Edit count required for eligibility', false, true );
+		$this->addOption( 'before', 'Consider edits made before DATE (strtotime format)', false, true );
+		$this->addOption( 'mainspace-only', 'Consider only NS_MAIN edits' );
+		$this->addOption(
+			'start-from',
+			'Start from user ID UID. Allows crashed invocations to be resumed.',
+			false,
+			true
+		);
+
+		$this->mBatchSize = 100;
+
+		$this->requireExtension( 'SecurePoll' );
 	}
 
-	$insertBatch = [];
-	foreach ( $res as $row ) {
-		$startBatch = $userId = $row->user_id;
-		$insertRow = [ 'li_name' => $listName, 'li_member' => $userId ];
-		if ( $minEdits === false ) {
-			$insertBatch[] = $insertRow;
-			continue;
+	public function execute() {
+		$dbr = wfGetDB( DB_REPLICA );
+		$dbw = wfGetDB( DB_MASTER );
+		$before = $this->hasOption( 'before' )
+			? $dbr->timestamp( strtotime( $this->getOption( 'before' ) ) ) : false;
+		$minEdits = $this->getOption( 'edits', false );
+
+		$listName = $this->getArg();
+		$startBatch = $this->getOption( 'start-from', 0 );
+		$insertOptions = [];
+
+		$listExists = $dbr->selectField(
+			'securepoll_lists',
+			'1',
+			[ 'li_name' => $listName ],
+			__METHOD__
+		);
+		if ( $listExists ) {
+			if ( $this->getOption( 'replace' ) ) {
+				$this->output( "Deleting existing list...\n" );
+				$dbw->delete( 'securepoll_lists', [ 'li_name' => $listName ], __METHOD__ );
+			} elseif ( $this->getOption( 'ignore-existing' ) ) {
+				$insertOptions[] = 'IGNORE';
+			} else {
+				$this->fatalError( "Error: list exists. Use --replace to replace it.\n" );
+			}
 		}
 
-		# Count edits
-		$edits = 0;
-		if ( class_exists( 'ActorMigration' ) ) {
-			$revWhere = ActorMigration::newMigration()
-				->getWhere( $dbr, 'rev_user', User::newFromRow( $row ) );
-		} else {
-			$revWhere = [
-				'tables' => [],
-				'orconds' => [ 'userid' => 'rev_user = ' . (int)$row->user_id ],
-				'joins' => [],
-			];
-		}
-		foreach ( $revWhere['orconds'] as $key => $cond ) {
-			$conds = [ $cond ];
-			if ( $before !== false ) {
-				$conds[] = ( $key === 'actor' ? 'revactor_timestamp' : 'rev_timestamp' ) . ' < ' . $beforeQ;
-			}
-			if ( isset( $options['mainspace-only'] ) ) {
-				$conds['page_namespace'] = 0;
-			}
+		$userQuery = User::getQueryInfo();
+		$userQuery['fields'] = [ 'user_id' ];
 
-			$edits = $dbr->selectRowCount(
-				[ 'revision', 'page' ] + $revWhere['tables'],
-				'1',
-				$conds,
-				$fname,
-				[ 'LIMIT' => $minEdits ],
-				$revWhere['joins'] + [ 'page' => [ 'INNER JOIN', 'rev_page = page_id' ] ]
+		$beforeQ = $before !== false ? $dbr->addQuotes( $before ) : false;
+
+		while ( true ) {
+			echo "user_id > $startBatch\n";
+			$res = $dbr->select(
+				$userQuery['tables'],
+				$userQuery['fields'],
+				[ 'user_id > ' . $dbr->addQuotes( $startBatch ) ],
+				__METHOD__,
+				[ 'LIMIT' => $this->mBatchSize ],
+				$userQuery['joins']
 			);
-		}
 
-		if ( $edits >= $minEdits ) {
-			$insertBatch[] = $insertRow;
+			if ( !$res->numRows() ) {
+				break;
+			}
+
+			$insertBatch = [];
+			foreach ( $res as $row ) {
+				$startBatch = $userId = $row->user_id;
+				$insertRow = [ 'li_name' => $listName, 'li_member' => $userId ];
+				if ( $minEdits === false ) {
+					$insertBatch[] = $insertRow;
+					continue;
+				}
+
+				# Count edits
+				$edits = 0;
+				$revWhere = ActorMigration::newMigration()
+					->getWhere( $dbr, 'rev_user', User::newFromRow( $row ) );
+
+				foreach ( $revWhere['orconds'] as $key => $cond ) {
+					$conds = [ $cond ];
+					if ( $before !== false ) {
+						$conds[] = ( $key === 'actor' ? 'revactor_timestamp' : 'rev_timestamp' )
+							. ' < ' . $beforeQ;
+					}
+					if ( $this->getOption( 'mainspace-only' ) ) {
+						$conds['page_namespace'] = 0;
+					}
+
+					$edits = $dbr->selectRowCount(
+						[ 'revision', 'page' ] + $revWhere['tables'],
+						'1',
+						$conds,
+						__METHOD__,
+						[ 'LIMIT' => $minEdits ],
+						$revWhere['joins'] + [ 'page' => [ 'INNER JOIN', 'rev_page = page_id' ] ]
+					);
+				}
+
+				if ( $edits >= $minEdits ) {
+					$insertBatch[] = $insertRow;
+				}
+			}
+			if ( $insertBatch ) {
+				$dbw->insert( 'securepoll_lists', $insertBatch, __METHOD__, $insertOptions );
+				wfWaitForSlaves();
+			}
 		}
 	}
-	if ( $insertBatch ) {
-		doInsert( $dbw, $insertBatch, $fname, $insertOptions );
-		wfWaitForSlaves();
-	}
+
 }
 
-/**
- * phan-taint-check gets confused with the other cli scripts due to globals
- *
- * @suppress SecurityCheck-SQLInjection
- * @param IDatabase $dbw
- * @param array $insertBatch
- * @param string $fname
- * @param array $insertOptions
- */
-function doInsert( $dbw, $insertBatch, $fname, $insertOptions ) {
-	$dbw->insert( 'securepoll_lists', $insertBatch, $fname, $insertOptions );
-}
+$maintClass = "MakeSimpleList";
+require_once RUN_MAINTENANCE_IF_MAIN;
