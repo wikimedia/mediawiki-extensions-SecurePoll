637ecfeae7a1aaaf423f11742b3e31d52cfc680f
James D. Forrester <jforrester@wikimedia.org>
2019-05-19 13:31:11 +0200

    build: Upgrade mediawiki/mediawiki-phan-config from 0.5.0 to 0.6.0
    Change-Id: Ie049465369b0b2b78eaeba71b44c1db19fc6228c

--- .phan/config.php
+++ .phan/config.php
@@ -3,6 +3,7 @@
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
 $cfg['file_list'][] = 'SecurePoll.constants.php';
+
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
