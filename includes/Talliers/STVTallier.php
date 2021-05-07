e5cde5dda727125c32896d93b8f021db26e1fcba
tsepothoabala <tthoabala@wikimedia.org>
2021-05-07 08:14:06 +0200

    Introduce a basic STVTallier class
    Bug: T282014
    Change-Id: Icd9f3dce04c82819beb6c33d8805d1be069b0b52

---
+++ includes/Talliers/STVTallier.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MediaWiki\Extensions\SecurePoll\Talliers;
+
+/**
+ * A STVTallier class,
+ * Currently work in progress see T282014.
+ */
+class STVTallier extends Tallier {
+
+	public function addVote( $scores ) {
+		// TODO: Implement addVote() method.
+	}
+
+	public function loadJSONResult( array $data ) {
+		// TODO: Implement loadJSONResult() method.
+	}
+
+	public function getJSONResult() {
+		// TODO: Implement getJSONResult() method.
+		return [];
+	}
+
+	public function getHtmlResult() {
+		// TODO: Implement getHtmlResult() method.
+	}
+
+	public function getTextResult() {
+		// TODO: Implement getTextResult() method.
+	}
+
+	public function finishTally() {
+		// TODO: Implement finishTally() method.
+	}
+}
