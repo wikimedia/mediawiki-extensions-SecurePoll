0ec19e13ddf7c3e6465b2377d4a06c51d89a8ccf
Thalia <thalia.e.chan@googlemail.com>
2021-05-07 09:07:09 +0100

    Add tests for abstract Tallier class
    Bug: T282011
    Change-Id: I22d301766d69324d0f11c32d887a6471ff256721

---
+++ tests/phpunit/unit/TallierTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace MediaWiki\Extensions\SecurePoll\Test\Unit;
+
+use MediaWiki\Extensions\SecurePoll\Entities\Option;
+use MediaWiki\Extensions\SecurePoll\Entities\Question;
+use MediaWiki\Extensions\SecurePoll\Talliers\ElectionTallier;
+use MediaWiki\Extensions\SecurePoll\Talliers\PluralityTallier;
+use MediaWiki\Extensions\SecurePoll\Talliers\Tallier;
+use MediaWikiUnitTestCase;
+use MWException;
+use RequestContext;
+
+/**
+ * @covers MediaWiki\Extensions\SecurePoll\Talliers\Tallier
+ */
+class TallierTest extends MediaWikiUnitTestCase {
+	private function getAbstractTallier() {
+		// Tallier constructor requires getOptions to return iterable
+		$question = $this->createMock( Question::class );
+		$question->method( 'getOptions' )
+			->willReturn( [] );
+
+		return $this->getMockForAbstractClass(
+			Tallier::class,
+			[
+				$this->createMock( RequestContext::class ),
+				$this->createMock( ElectionTallier::class ),
+				$question,
+			]
+		);
+	}
+
+	public function testFactoryValidType() {
+		// Tallier constructor requires getOptions to return iterable
+		$question = $this->createMock( Question::class );
+		$question->method( 'getOptions' )
+			->willReturn( [] );
+
+		$tallier = Tallier::factory(
+			$this->createMock( RequestContext::class ),
+			'plurality',
+			$this->createMock( ElectionTallier::class ),
+			$question
+		);
+		$this->assertInstanceOf( PluralityTallier::class, $tallier );
+	}
+
+	public function testFactoryInvalidType() {
+		$this->expectException( MWException::class );
+		$tallier = Tallier::factory(
+			$this->createMock( RequestContext::class ),
+			'invalid',
+			$this->createMock( ElectionTallier::class ),
+			$this->createMock( Question::class )
+		);
+	}
+
+	public function testConvertRanksToHtml() {
+		$ranks = [
+			0 => 1,
+			1 => 2,
+			2 => 3,
+			3 => 4,
+		];
+
+		// parseMessage is called on each option
+		$options = array_map( function ( $value ) {
+			$option = $this->createMock( Option::class );
+			$option->method( 'parseMessage' )
+				->willReturn( $value );
+			return $option;
+		}, [ 'A', 'C', 'B', 'D' ] );
+
+		$tallier = $this->getAbstractTallier();
+		$tallier->optionsById = $options;
+
+		$html = "<table class=\"securepoll-table\"><tr><td>1</td><td>A</td></tr>\n" .
+			"<tr><td>2</td><td>C</td></tr>\n" .
+			"<tr><td>3</td><td>B</td></tr>\n" .
+			"<tr><td>4</td><td>D</td></tr>\n" .
+			"</table>";
+
+		$this->assertSame( $html, $tallier->convertRanksToHtml( $ranks ) );
+	}
+
+	public function testConvertRanksToText() {
+		$ranks = [
+			0 => 1,
+			1 => 2,
+			2 => 3,
+			3 => 4,
+		];
+
+		// getMessage is called on each option
+		$options = array_map( function ( $value ) {
+			$option = $this->createMock( Option::class );
+			$option->method( 'getMessage' )
+				->willReturn( $value );
+			return $option;
+		}, [ 'A', 'C', 'B', 'D' ] );
+
+		$tallier = $this->getAbstractTallier();
+		$tallier->optionsById = $options;
+
+		$text = "1      | A\n" .
+			"2      | C\n" .
+			"3      | B\n" .
+			"4      | D\n";
+
+		$this->assertSame( $text, $tallier->convertRanksToText( $ranks ) );
+	}
+
+}
