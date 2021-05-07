7c8ec4490654c90ffd6ba603b8fb8f033ae8f120
AnaïsGueyte <agueyte@wikimedia.org>
2021-05-07 15:37:04 -0700

    Add unit tests for Ballot class
    Add unit test for abstract class Ballot and subclasses
    
    Bug: T282010
    Change-Id: Ida8e1f4e03fec34d19d751da469e5ad77f9eb480

---
+++ tests/phpunit/unit/BallotTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace MediaWiki\Extensions\SecurePoll\Test\Unit;
+
+use MediaWiki\Extensions\SecurePoll\Ballots\Ballot;
+use MediaWiki\Extensions\SecurePoll\Ballots\BallotStatus;
+use MediaWiki\Extensions\SecurePoll\Entities\Election;
+use MediaWiki\Extensions\SecurePoll\Entities\Option;
+use MediaWiki\Extensions\SecurePoll\Entities\Question;
+use MediaWikiUnitTestCase;
+use RequestContext;
+
+/**
+ * @covers MediaWiki\Extensions\SecurePoll\Ballots\Ballot
+ */
+class BallotTest extends MediaWikiUnitTestCase {
+	private function getAbstractBallot( $election = null ) {
+		$election = $election ?? $this->createMock( Election::class );
+		$ballot = $this->getMockForAbstractClass(
+			Ballot::class,
+			[
+				$this->createMock( RequestContext::class ),
+				$election,
+			]
+		);
+
+		return $ballot;
+	}
+
+	public function testGetForm() {
+		$prevStatus = $this->createMock( BallotStatus::class );
+		$prevStatus->method( 'sp_getHTML' )->willReturn( '' );
+
+		$option = $this->createMock( Option::class );
+
+		$question = $this->createMock( Question::class );
+		$question->method( 'getOptions' )->willReturn( [ $option ] );
+		$question->method( 'parseMessage' )->willReturn( '' );
+
+		$election = $this->createMock( Election::class );
+		$election->method( 'getQuestions' )->willReturn( [ $question ] );
+
+		$ballot = $this->getAbstractBallot( $election );
+		$ballot->method( 'getQuestionForm' )
+			->willReturn( $this->createMock( \OOUI\FieldsetLayout::class ) );
+
+		$form = $ballot->getForm();
+		$this->assertIsArray( $form );
+		$this->assertIsArray( $ballot->getForm( false ) );
+		$this->assertIsArray( $ballot->getForm( $prevStatus ) );
+		$this->assertCount( 1, $form );
+		$this->assertInstanceOf( \OOUI\Element::class, $form[0] );
+	}
+}
