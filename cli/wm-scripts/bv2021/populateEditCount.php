f89c0772191e10a5b33be2dee029d1ded17b9c8b
Tim Starling <tstarling@wikimedia.org>
2021-08-03 16:45:07 +1000

    Add scripts for 2021 voter qualification
    I updated the way that user information is queried. The idea of the
    original code is that the user cache speeds up isAnon(), and
    User::getId() is fast in any case. But bitrot meant that in the worst
    case it was doing two queries: one from User::loadFromDatabase() and
    another from findActorIdInternal(). Maybe a join would be better, but
    this way is simple and likely to work for another couple of years.
    
    Change-Id: I9d9e5da022f41fcc70785af67625364d2ccefbdb

---
+++ cli/wm-scripts/bv2021/populateEditCount.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * You may vote from any one registered account you own on a Wikimedia wiki.
+ * You may only vote once, regardless of how many accounts you own.
+ * To qualify, this one account must:
+ *   - blocked in no more than one project;
+ *   - and not be a bot;
+ *   - and have made at least 300 edits before 5 July 2021 across Wikimedia wikis;
+ *   - and have made at least 20 edits between 5 January 2021 and 5 July 2021.
+ */
+
+use MediaWiki\MediaWikiServices;
+
+require __DIR__ . '/../../cli.inc';
+
+$dbr = wfGetDB( DB_REPLICA );
+$dbw = wfGetDB( DB_PRIMARY );
+
+$maxUser = $dbr->selectField( 'user', 'MAX(user_id)', false );
+$beforeTime = $dbr->addQuotes( '20210705000000' );
+$betweenTime = [
+	$dbr->addQuotes( '20210105000000' ),
+	$dbr->addQuotes( '20210705000000' )
+];
+$fname = 'populateEditCount';
+
+$numUsers = 0;
+$services = MediaWikiServices::getInstance();
+$actorMigration = $services->getActorMigration();
+$actorNormalization = $services->getActorNormalization();
+
+for ( $userId = 1; $userId <= $maxUser; $userId++ ) {
+	// Find actor ID
+	$row = $dbr->newSelectQueryBuilder()
+		->select( [ 'actor_id', 'actor_name', 'actor_user' ] )
+		->from( 'actor' )
+		->where( [ 'actor_user' => $userId ] )
+		->caller( __METHOD__ )
+		->fetchRow();
+	if ( !$row ) {
+		continue;
+	}
+	$user = $actorNormalization->newActorFromRow( $row );
+
+	$longEdits = 0;
+	$shortEdits = 0;
+
+	$revWhere = $actorMigration->getWhere( $dbr, 'rev_user', $user );
+
+	foreach ( $revWhere['orconds'] as $key => $cond ) {
+		$tsField = $key === 'actor' ? 'revactor_timestamp' : 'rev_timestamp';
+
+		$longEdits += $dbr->selectField(
+			[ 'revision' ] + $revWhere['tables'],
+			'COUNT(*)',
+			[
+				$cond,
+				$tsField . ' < ' . $beforeTime
+			],
+			$fname,
+			[],
+			$revWhere['joins']
+		);
+
+		$shortEdits += $dbr->selectField(
+			[ 'revision' ] + $revWhere['tables'],
+			'COUNT(*)',
+			[
+				$cond,
+				$tsField . ' BETWEEN ' . $betweenTime[0] . ' AND ' . $betweenTime[1]
+			],
+			$fname,
+			[],
+			$revWhere['joins']
+		);
+	}
+
+	if ( $longEdits != 0 || $shortEdits != 0 ) {
+		$dbw->insert( 'bv2021_edits',
+			[
+				'bv_user' => $userId,
+				'bv_long_edits' => $longEdits,
+				'bv_short_edits' => $shortEdits
+			],
+			$fname
+		);
+		$numUsers++;
+	}
+}
+
+echo wfWikiID() . ": $numUsers users added\n";
