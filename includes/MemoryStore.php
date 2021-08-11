157028d8858e5d6f7f99e8f08b7e7461100265b6
STran <stran@wikimedia.org>
2021-08-10 21:40:07 -0700

    Add ad-hoc logging to tally process
    - Log where an election is after it starts running
    - Log updates to the tallying votes process (in increments of 5)
    - Log all steps of GPG decryption since that's historically failed
      and/or timed out in almost every step one time or another
    
    Bug: T288366
    Change-Id: Ic9891dfbffff71e7cc614d5cea95e3693beede06

--- includes/MemoryStore.php
+++ includes/MemoryStore.php
@@ -2,6 +2,7 @@
 
 namespace MediaWiki\Extensions\SecurePoll;
 
+use MediaWiki\Logger\LoggerFactory;
 use MWException;
 use Status;
 
@@ -100,10 +101,33 @@ class MemoryStore implements Store {
 		if ( !isset( $this->votes[$electionId] ) ) {
 			return Status::newGood();
 		}
-		foreach ( $this->votes[$electionId] as $vote ) {
+
+		// T288366 Tallies fail on beta/prod with little visibility
+		// Add logging to gain more context into where it fails
+		LoggerFactory::getInstance( 'AdHocDebug' )->info(
+			'Adding votes to election',
+			[
+				'electionId' => $electionId,
+				'voteCount' => count( $this->votes[$electionId] ),
+			]
+		);
+
+		foreach ( $this->votes[$electionId] as $i => $vote ) {
 			$status = call_user_func( $callback, $this, $vote );
 			if ( !$status->isOK() ) {
 				return $status;
+			}
+
+			if ( $i % 5 === 0 ) {
+				// T288366 Tallies fail on beta/prod with little visibility
+				// Add logging to gain more context into where it fails
+				LoggerFactory::getInstance( 'AdHocDebug' )->info(
+					'Batch addRecord() currently in progress',
+					[
+						'electionId' => $electionId,
+						'currentVoteIndex' => $i,
+					]
+				);
 			}
 		}
 
