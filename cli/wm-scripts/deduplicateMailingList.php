7637495ece937f136e184c7744baec37d6bc197a
Tim Starling <tstarling@wikimedia.org>
2021-08-12 16:21:29 +1000

    New CLI scripts makeMailingList.php and deduplicateMailingList.php
    * Rewrite bv2013/doSpam.php from scratch with a different name.
    * Fully check voter qualifications, so bots etc. are not mailed.
    * Running the script on the user's local wiki makes it easier to get
      configuration data such as the site name. Deduplication with selection
      of the active wiki is split out to a separate simple step.
    * Load the language preference properly, so that global preferences are
      respected.
    * Parameters are loaded from an election specified in command line
      options, so the script can be used for multiple years.
    
    Change-Id: I71c7419a67536704f6f76c5967a5e00abc37393b

---
+++ cli/wm-scripts/deduplicateMailingList.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace MediaWiki\Extensions\SecurePoll;
+
+use Generator;
+use InvalidArgumentException;
+use Maintenance;
+
+if ( getenv( 'MW_INSTALL_PATH' ) ) {
+	$IP = getenv( 'MW_INSTALL_PATH' );
+} else {
+	$IP = __DIR__ . '/../../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+require __DIR__ . '/includes/MailingListEntry.php';
+
+/**
+ * Deduplicate a set of mailing lists produced by makeMailingList.php. If
+ * multiple users from different wikis are present, choose the one with the
+ * greatest edit count.
+ *
+ * The arguments are a list of mailing list files. If there are no arguments,
+ * a mailing list is read from stdin.
+ */
+class DeduplicateMailingList extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( 'output-file',
+			'The output file name',
+			false, true, 'o' );
+		$this->addArg( 'input-file', 'The input file or a list of files', false );
+	}
+
+	public function execute() {
+		if ( $this->hasOption( 'output-file' ) ) {
+			$outFile = fopen( $this->getOption( 'output-file' ), 'w' );
+			if ( !$outFile ) {
+				$this->fatalError( "Unable to open output file" );
+			}
+		} else {
+			$outFile = STDOUT;
+		}
+		$entries = [];
+		/** @var MailingListEntry $newEntry */
+		foreach ( $this->readEntries() as $newEntry ) {
+			$name = $newEntry->userName;
+			if ( !isset( $entries[$name] )
+				|| $newEntry->editCount > $entries[$name]->editCount
+			) {
+				$entries[$name] = $newEntry;
+			}
+		}
+		foreach ( $entries as $entry ) {
+			fwrite( $outFile, $entry->toString() );
+		}
+	}
+
+	/**
+	 * Read entries from the input files, returning an iterator over
+	 * MailingListEntry objects
+	 *
+	 * @return Generator|iterable<MailingListEntry>
+	 */
+	private function readEntries() {
+		$numArgs = count( $this->mArgs );
+		if ( !$numArgs ) {
+			$this->error( "Reading from stdin..." );
+			foreach ( $this->readEntriesFromFile( STDIN, 'stdin' ) as $entry ) {
+				yield $entry;
+			}
+		} else {
+			for ( $fileIndex = 0; $fileIndex < count( $this->mArgs ); $fileIndex++ ) {
+				$fileName = $this->getArg( $fileIndex );
+				$file = fopen( $fileName, 'r' );
+				if ( !$file ) {
+					$this->fatalError( "Unable to open input file \"$fileName\"" );
+				}
+				foreach ( $this->readEntriesFromFile( $file, $fileName ) as $entry ) {
+					yield $entry;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Read entries from one file-like resource.
+	 *
+	 * @param resource $file
+	 * @param string $fileName
+	 * @return Generator|iterable<MailingListEntry>
+	 */
+	private function readEntriesFromFile( $file, $fileName ) {
+		for ( $lineNum = 1; true; $lineNum++ ) {
+			$line = fgets( $file );
+			if ( $line === false ) {
+				break;
+			}
+			if ( $line === "\n" ) {
+				continue;
+			}
+			try {
+				$entry = MailingListEntry::newFromString( $line );
+				yield $entry;
+			} catch ( InvalidArgumentException $e ) {
+				$this->error( "Skipping invalid entry in file $fileName line $lineNum" );
+			}
+		}
+	}
+}
+
+$maintClass = DeduplicateMailingList::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
---
+++ cli/wm-scripts/includes/MailingListEntry.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace MediaWiki\Extensions\SecurePoll;
+
+class MailingListEntry {
+	/** @var string */
+	public $wiki;
+	/** @var string */
+	public $siteName;
+	/** @var string */
+	public $userName;
+	/** @var string */
+	public $email;
+	/** @var string */
+	public $language;
+	/** @var string|int */
+	public $editCount;
+
+	public static function newFromString( $str ) {
+		$fields = explode( "\t", rtrim( $str, "\n" ) );
+		if ( count( $fields ) !== 6 ) {
+			throw new \InvalidArgumentException( 'Invalid mailing list entry' );
+		}
+		$entry = new self;
+		$entry->wiki = $fields[0];
+		$entry->siteName = $fields[1];
+		$entry->userName = $fields[2];
+		$entry->email = $fields[3];
+		$entry->language = $fields[4];
+		$entry->editCount = $fields[5];
+		return $entry;
+	}
+
+	public function toString() {
+		return implode( "\t",
+			[
+				$this->wiki,
+				$this->siteName,
+				$this->userName,
+				$this->email,
+				$this->language,
+				$this->editCount
+			]
+		) . "\n";
+	}
+}
