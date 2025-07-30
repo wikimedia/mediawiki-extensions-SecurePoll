99d7b0c0cd483d8fa3cb5b88b6b3a9032d00eb92
Novem Linguae <novemlinguae@gmail.com>
2025-07-30 04:10:24 -0700

    DropUnusedTables: add --dry-run option
    Why
    - Requested by DBA to generate a list of tables to drop that can
      be copy/pasted into a different tool
    
    What
    - Add `--dry-run` option to maintenance/DropUnusedTables.php
    - Also mention the database name, in case this maintenance script
      is run on multiple wikis at once. I feel like having the database name
      right next to the table name would be useful console output.
    - Add a @var so that VS Code provides correct IntelliSense for that
      variable.
    
    Testing notes
    - `docker compose exec mediawiki php maintenance/run.php SecurePoll:DropUnusedTables --dry-run`
    
    Bug: T395928
    Change-Id: I5eabd7cadfac74996e22072997984c0708769272

--- maintenance/DropUnusedTables.php
+++ maintenance/DropUnusedTables.php
@@ -2,6 +2,7 @@
 
 namespace MediaWiki\Extension\SecurePoll\Maintenance;
 
+use MediaWiki\MainConfigNames;
 use MediaWiki\Maintenance\Maintenance;
 
 // @codeCoverageIgnoreStart
@@ -31,6 +32,7 @@ class DropUnusedTables extends Maintenance {
 		parent::__construct();
 		$this->addDescription( 'Drop unused tables' );
 		$this->addOption( 'force', 'Drop tables even if they are non-empty' );
+		$this->addOption( 'dry-run', 'Run the script in read-only mode. Don\'t drop any tables.' );
 		$this->requireExtension( 'SecurePoll' );
 	}
 
@@ -43,6 +45,7 @@ class DropUnusedTables extends Maintenance {
 			return;
 		}
 
+		/** @var \Wikimedia\Rdbms\IMaintainableDatabase $dbw */
 		$dbw = $this->getPrimaryDB();
 		'@phan-var \Wikimedia\Rdbms\IMaintainableDatabase $dbw';
 
@@ -55,11 +58,17 @@ class DropUnusedTables extends Maintenance {
 					->caller( __METHOD__ )
 					->fetchField();
 
+				$database = $this->getConfig()->get( MainConfigNames::DBname );
 				if ( !$value || $this->hasOption( 'force' ) ) {
-					$this->output( "Dropping table '$table'\n" );
-					$dbw->dropTable( $table, __METHOD__ );
+					if ( $this->hasOption( 'dry-run' ) ) {
+						$this->output( "Would have dropped table $database.$table, but we are " .
+							"in dry-run mode.\n" );
+					} else {
+						$this->output( "Dropped table $database.$table\n" );
+						$dbw->dropTable( $table, __METHOD__ );
+					}
 				} else {
-					$this->output( "Table '$table' is not empty, skipping.\n" );
+					$this->output( "Table $database.$table is not empty, skipping.\n" );
 				}
 			}
 		}
