5361579c7faf00bac6f70c77679b59417f95fbee
Foks <jsutherland@wikimedia.org>
2026-04-15 14:59:30 -0700

    SecurePoll: Adding scripts for U4C votes
    Same eligibility for each vote.
    
    Bug: T423528
    Change-Id: Iaf7caecce4b864977358aef24634b7c28f7c3572

---
+++ maintenance/wm-scripts/ucoc2026/ucoc2026PopulateEditCount.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Rules: https://w.wiki/LBSx
+ * You may vote from any one registered account you own on a Wikimedia wiki.
+ * You may only vote once, regardless of how many accounts you own.
+ * To qualify, this one account must:
+ *   - not be blocked in more than one project;
+ *   - and not be a bot;
+ *   - and have made at least 300 edits before 19 February 2026 across Wikimedia wikis;
+ *   - and have made at least 20 edits between 19 August 2025 and 19 February 2026.
+ */
+
+// @codeCoverageIgnoreStart
+require_once dirname( __DIR__ ) . '/PopulateEditCount.php';
+// @codeCoverageIgnoreEnd
+
+use MediaWiki\Extension\SecurePoll\PopulateEditCount;
+
+class Ucoc2026PopulateEditCount extends PopulateEditCount {
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( 'Populates the securepoll_ucoc2026_edits table for the UCoC annual review and U4C elections' );
+		$this->table = 'securepoll_ucoc2026_edits';
+		$this->beforeTime = '20260219000000';
+		$this->beforeEditsToCount = 300;
+		$this->betweenStart = '20250819000000';
+		$this->betweenEnd = '20260219000000';
+		$this->betweenEditsToCount = 20;
+	}
+}
+
+// @codeCoverageIgnoreStart
+$maintClass = Ucoc2026PopulateEditCount::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
+// @codeCoverageIgnoreEnd
---
+++ maintenance/wm-scripts/ucoc2026/ucoc2026_tables.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE securepoll_ucoc2026_edits (
+	bv_user int not null primary key,
+	bv_long_edits int not null,
+	bv_short_edits int not null
+);
