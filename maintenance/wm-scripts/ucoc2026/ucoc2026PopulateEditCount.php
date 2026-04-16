f55f19a59bfd212b0f0bcadeac9fc51944f261fc
foks <jsutherland@wikimedia.org>
2026-04-16 08:46:30 -0700

    SecurePoll: Updating dates for UCoC votes
    Dates were tweaked since the previous commit.
    
    Bug: T423528
    Change-Id: I9e9e8d1de0f39a4ca3122981e771643c2baec68b

--- maintenance/wm-scripts/ucoc2026/ucoc2026PopulateEditCount.php
+++ maintenance/wm-scripts/ucoc2026/ucoc2026PopulateEditCount.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Rules: https://w.wiki/LBSx
+ * Rules: https://w.wiki/LJWb
  * You may vote from any one registered account you own on a Wikimedia wiki.
  * You may only vote once, regardless of how many accounts you own.
  * To qualify, this one account must:
  *   - not be blocked in more than one project;
  *   - and not be a bot;
- *   - and have made at least 300 edits before 19 February 2026 across Wikimedia wikis;
- *   - and have made at least 20 edits between 19 August 2025 and 19 February 2026.
+ *   - and have made at least 300 edits before 24 April 2026 across Wikimedia wikis;
+ *   - and have made at least 20 edits between 24 October 2025 and 24 April 2026.
  */
 
 // @codeCoverageIgnoreStart
@@ -22,10 +22,10 @@ class Ucoc2026PopulateEditCount extends PopulateEditCount {
 		parent::__construct();
 		$this->addDescription( 'Populates the securepoll_ucoc2026_edits table for the UCoC annual review and U4C elections' );
 		$this->table = 'securepoll_ucoc2026_edits';
-		$this->beforeTime = '20260219000000';
+		$this->beforeTime = '20260424000000';
 		$this->beforeEditsToCount = 300;
-		$this->betweenStart = '20250819000000';
-		$this->betweenEnd = '20260219000000';
+		$this->betweenStart = '20251024000000';
+		$this->betweenEnd = '20260424000000';
 		$this->betweenEditsToCount = 20;
 	}
 }
